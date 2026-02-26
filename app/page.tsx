00d60509fa39ab3536b22a24a680b0d540eba31b
Lucky-1803 <lucky18032@gmail.com>
2026-02-26 16:47:03 +0530

    Refactor Home component for session handling
    Update Home component to handle server session and redirects.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
+import { getServerSession } from "next-auth"
+import { authOptions } from "./api/auth/[...nextauth]/route"
+import { redirect } from "next/navigation"
 
-export default function Home() {
-  return 
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+
+  if (session) {
+    redirect("/dashboard")
+  }
+
+  redirect("/login")
 }
